015f258611e9bef8d32b9f4f8d9e8f9db47e1166
zhaojian <1229108012@qq.com>
2018-08-30 20:58:10 +0800

    1

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -49,14 +49,14 @@ function startHeartAnimation() {
 	var animationTimer = setInterval(function () {
 		var bloom = getHeartPoint(angle);
 		var draw = true;
-		for (var i = 0; i < heart.length; i++) {
-			var p = heart[i];
-			var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
-			if (distance < Garden.options.bloomRadius.max * 1.3) {
-				draw = false;
-				break;
-			}
-		}
+		// for (var i = 0; i < heart.length; i++) {
+		// 	var p = heart[i];
+		// 	var distance = Math.sqrt(Math.pow(p[0] - bloom[0], 2) + Math.pow(p[1] - bloom[1], 2));
+		// 	if (distance < Garden.options.bloomRadius.max * 1.3) {
+		// 		draw = false;
+		// 		break;
+		// 	}
+		// }
 		if (draw) {
 			heart.push(bloom);
 			garden.createRandomBloom(bloom[0], bloom[1]);
